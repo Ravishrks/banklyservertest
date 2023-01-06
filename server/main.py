0bed66e05318bd08ac45e9a3846dfd5d730d027c
Ravish <oceancusp@gmail.com>
2023-01-06 14:00:48 +0000

    enc desc implemented using AES

--- server/main.py
+++ server/main.py
@@ -1,6 +1,13 @@
-from typing import Union
+import json
 
 from fastapi import FastAPI
+import aiohttp
+
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+from base64 import b64encode, b64decode
+
 
 app = FastAPI()
 
@@ -13,3 +20,61 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/send-payload/")
+async def send_xml_api_request():
+
+    async with aiohttp.ClientSession() as session:
+        api_key = 'xUHvlTOtkLn37jnuG0Yp8zr2kivgRg6j'
+        header = {'Content-Type': 'application/xml',
+                  "apikey": api_key, "SrcApp": ''}
+        payload = """<xml>
+                <ReferenceNumber>20190704000084</ReferenceNumber>
+                <MerchantId>FLP0000001</MerchantId>
+                <MerchantPassword>admin12345</MerchantPassword>
+                <Product>VV01</Product>
+                <ProductCategory>1</ProductCategory>
+                <MobileNumber>9944838952</MobileNumber>
+                <TransactionRemark>FLIPKART Card Mobile Number
+                link</TransactionRemark>
+                </xml>"""
+
+        # Encrypting data
+        key = get_random_bytes(16)
+        cipher = AES.new(key, AES.MODE_ECB)
+        ct_bytes = cipher.encrypt(pad(payload, AES.block_size))
+        iv = b64encode(cipher.iv).decode('utf-8')
+        encrypted_Data = b64encode(ct_bytes).decode('utf-8')
+
+        data = {
+            "requestId": "",
+            "service": "Bankly",
+            "encryptedKey": key,
+            "oaepHashingAlgorithm": "NONE",
+            "iv": iv,
+            "encryptedData": encrypted_Data,
+            "clientInfo": "",
+            "optionalParam": ""
+
+
+        }
+
+        endpoint_url = 'https://apibankingonesandbox.icicibank.com/api/v1/pcms-chw?service=LinkedMobile'
+
+        async with session.post(endpoint_url, headers=header, data=data) as response:
+            #  decrypting response
+
+            try:
+                b64 = json.loads(response)
+                iv = b64decode(b64['iv'])
+
+                # getFirst16Bytes(Base64Decode(encryptedData)
+                ct = b64decode(b64['encryptedData'])[:16]
+                cipher = AES.new(key, AES.MODE_ECB, iv)
+                pt = unpad(cipher.decrypt(ct), AES.block_size)
+                print("The message was: ", pt)
+            except (ValueError, KeyError):
+                print("Incorrect decryption")
+
+    return {"data": response.headers, }
---
+++ server/readme.md
@@ -0,0 +1,21 @@
+# Bankly test server
+
+## Start server
+
+'''bash
+uvicorn main:app --reload --host 0.0.0.0 --port 80
+'''
+
+## Using AES for encryption
+
+We will use below package
+
+'''bash
+pip install pycryptodome
+'''
+
+## Handeling Requests in async way
+
+'''bash
+pip install aiohttp 3.8.3
+'''
