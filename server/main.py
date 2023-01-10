b128b5d46531c52e24a10dfcc0a139d9e2737b13
Ravish <oceancusp@gmail.com>
2023-01-10 19:41:16 +0000

    sync

--- server/main.py
+++ server/main.py
@@ -39,14 +39,13 @@ async def send_monthly_statement_api_request():
     async with aiohttp.ClientSession() as session:
         # Generate random string for request id
         requist_id = ''.join(random.SystemRandom().choice(
-            string.ascii_uppercase + string.digits) for _ in range(32))
+            string.ascii_uppercase + string.digits) for _ in range(16))
 
         header = {'Content-Type': 'application/json',
                   "apikey": API_TEST_KEY, "SrcApp": SRC_APP}
 
         # Business Data to be send to ICICI server
-        payload = "<xml><ReferenceNumber>20190704000084</ReferenceNumber><MerchantId>FLP0000001</MerchantId><MerchantPassword>admin12345</MerchantPassword><Product>VV01</Product><ProductCategory>1</ProductCategory><MobileNumber>9944838952</MobileNumber><TransactionRemark>FLIPKART Card Mobile Number link</TransactionRemark></xml>".encode(
-            "utf-8")
+        payload = "<xml><ReferenceNumber>20190704000084</ReferenceNumber><MerchantId>FLP0000001</MerchantId><MerchantPassword>admin12345</MerchantPassword><Product>VV01</Product><ProductCategory>1</ProductCategory><MobileNumber>9944838952</MobileNumber><TransactionRemark>FLIPKART Card Mobile Number link</TransactionRemark></xml>"
 
         # We have to encrypt key using ICICI's public key,
         # ICICI will use it's private key to decrypt key and use decrypted
@@ -55,14 +54,16 @@ async def send_monthly_statement_api_request():
         # key to be used to encrypt payload
         session_key = get_random_bytes(16)
         cipher_aes = AES.new(session_key, AES.MODE_CBC)
-        ct_bytes = cipher_aes.encrypt(pad(payload, AES.block_size))
+        ct_bytes = cipher_aes.encrypt(
+            pad(bytes(payload, 'utf-8'), AES.block_size))
         iv = b64encode(cipher_aes.iv).decode('utf-8')
 
         # Reading RSA key from stored file
         recipient_key = RSA.import_key(open("ICICIUAT.cer").read())
 
         # Encrypt the session key with the public RSA key
-        cipher_rsa = PKCS1_OAEP.new(recipient_key,hashAlgo=SHA384)  # Default is SHA1
+        cipher_rsa = PKCS1_OAEP.new(
+            recipient_key)  # Default is SHA1
         enc_session_key = cipher_rsa.encrypt(session_key)
 
         request_data = {
